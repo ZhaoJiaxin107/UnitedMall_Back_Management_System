a130c8d7be60e94bb71cb612a157e738e53040b8
Jiaxin Zhao <jzhao537@uwo.ca>
2021-01-26 19:11:24 +0800

    commit form validate

--- UMall_Admin/src/pages/login/Index.vue
+++ UMall_Admin/src/pages/login/Index.vue
@@ -2,15 +2,15 @@
   <div class = "main">
     <div class="loginform">
         <h3 class="title">管理员登录</h3>
-        <el-form ref="form" :model="loginInfo">
-          <el-form-item label="">
+        <el-form ref="form" :model="loginInfo" :rules = "rules">
+          <el-form-item label="" prop = "username">
             <el-input v-model="loginInfo.username" placeholder="请输入账号" clearable>
                 <template slot="prepend">
                   <i class="el-icon-s-custom"></i>
                 </template>
             </el-input>
           </el-form-item>
-          <el-form-item label="">
+          <el-form-item label="" prop = "password">
             <el-input v-model="loginInfo.password" placeholder="请输入密码" show-password clearable>
                 <template slot="prepend">
                   <i class="el-icon-lock"></i>
@@ -32,12 +32,33 @@ export default {
       loginInfo: {
         username: '',
         password: ''
+      },
+      rules: {
+        // 规则的编写
+        username: [
+          { required: true, message: '请输入账号', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' }
+        ]
       }
     }
   },
   methods: {
     onSubmit () {
-      console.log('on submit')
+    //   console.log('on submit')
+      // 触发表单验证 调用Form组件的validate回调方法
+      this.$refs.form.validate(valid => {
+        // validate表示验证是否全部通过
+        // console.log('valid', valid)
+        if (valid) {
+        // 全部通过
+          this.login()
+        }
+      })
+    },
+    login () {
+      console.log('login')
     }
   }
 }
