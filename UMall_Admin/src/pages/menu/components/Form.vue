4f1afce73906881be4393daed128e7f5859b4e77
Jiaxin Zhao <jzhao537@uwo.ca>
2021-01-29 11:49:07 +0800

    update form.vue

--- UMall_Admin/src/pages/menu/components/Form.vue
+++ UMall_Admin/src/pages/menu/components/Form.vue
@@ -3,55 +3,81 @@
        title: 对话框标题
        visible: 是否显示
    -->
-  <el-dialog :title="title" :visible.sync="dialogFormVisible" @close = "clearForm">
-  <el-form :model="form" ref="form" :rules = "rules" label-suffix="：" label-width="120px">
-    <el-form-item :label="form.type === 1 ? '目录名称': '菜单名称'" prop = "title">
-      <el-input v-model.trim="form.title" autocomplete="off" placeholder="请输入名称">
-      </el-input>
-    </el-form-item>
-    <el-form-item label="上级菜单">
-      <el-select v-model="form.pid" placeholder="请选择上级菜单">
-        <!-- 当value与v-model的值相等的时候就选中 -->
-        <!-- value与v-model是全等比较 -->
-        <el-option label="顶级菜单" :value="0"></el-option>
-        <!-- 循环一级菜单 -->
-        <el-option v-for = "item of firstMenuList" :key="item.id" :label="' |- '+item.title"
-        :value="item.id">
-        </el-option>
-      </el-select>
-    </el-form-item>
-    <el-form-item label = "菜单类型">
+  <el-dialog
+    :title="title"
+    :visible.sync="dialogFormVisible"
+    @close="clearForm"
+  >
+    <el-form
+      :model="form"
+      ref="form"
+      :rules="rules"
+      label-suffix="："
+      label-width="120px"
+    >
+      <el-form-item
+        :label="form.type === 1 ? '目录名称' : '菜单名称'"
+        prop="title"
+      >
+        <el-input
+          v-model.trim="form.title"
+          autocomplete="off"
+          placeholder="请输入名称"
+        >
+        </el-input>
+      </el-form-item>
+      <el-form-item label="上级菜单">
+        <el-select v-model="form.pid" placeholder="请选择上级菜单">
+          <!-- 当value与v-model的值相等的时候就选中 -->
+          <!-- value与v-model是全等比较 -->
+          <el-option label="顶级菜单" :value="0"></el-option>
+          <!-- 循环一级菜单 -->
+          <el-option
+            v-for="item of firstMenuList"
+            :key="item.id"
+            :label="' |- ' + item.title"
+            :value="item.id"
+          >
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="菜单类型">
         <!-- 单选框组 -->
         <el-radio-group v-model="form.type">
-            <el-radio :label="1">目录</el-radio>
-            <el-radio :label="2">菜单</el-radio>
+          <el-radio :label="1">目录</el-radio>
+          <el-radio :label="2">菜单</el-radio>
         </el-radio-group>
-    </el-form-item>
-    <el-form-item v-show = "form.type === 1" label="目录图标" prop="icon">
-      <el-input v-model="form.icon" autocomplete="off" placeholder="请输入目录图标">
-      </el-input>
-    </el-form-item>
-    <el-form-item v-show = "form.type === 2" label="菜单地址" prop = "url">
-      <el-input v-model.trim="form.url" autocomplete="off" placeholder="请输入菜单地址">
-      </el-input>
-    </el-form-item>
-    <el-form-item label="状态">
-       <!-- 开关组件
+      </el-form-item>
+      <el-form-item v-show="form.type === 1" label="目录图标" prop="icon">
+        <el-input
+          v-model="form.icon"
+          autocomplete="off"
+          placeholder="请输入目录图标"
+        >
+        </el-input>
+      </el-form-item>
+      <el-form-item v-show="form.type === 2" label="菜单地址" prop="url">
+        <el-input
+          v-model.trim="form.url"
+          autocomplete="off"
+          placeholder="请输入菜单地址"
+        >
+        </el-input>
+      </el-form-item>
+      <el-form-item label="状态">
+        <!-- 开关组件
             active-value: Boolean | string | number 选中时的值
             inactive-value: Boolean | string | number 未选中时的值
         -->
-       <el-switch
-       v-model="form.status"
-       :active-value="1"
-       :inactive-value="2">
-       </el-switch>
-    </el-form-item>
-    <el-form-item>
-       <el-button @click="dialogFormVisible = false">取 消</el-button>
-       <el-button type="primary" @click="onSubmit">确 定</el-button>
-    </el-form-item>
-  </el-form>
-</el-dialog>
+        <el-switch v-model="form.status" :active-value="1" :inactive-value="2">
+        </el-switch>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="onSubmit">确 定</el-button>
+      </el-form-item>
+    </el-form>
+  </el-dialog>
 </template>
 
 <script>
@@ -102,17 +128,11 @@ export default {
     return {
       dialogFormVisible: false,
       title: '添加菜单', // 对话框的标题
-      form: {...defaultForm},
+      form: { ...defaultForm },
       rules: {
-        title: [
-          { required: true, message: '请输入名称', trigger: 'blur' }
-        ],
-        icon: [
-          { validator: checkIcon, trigger: 'blur' }
-        ],
-        url: [
-          { validator: checkUrl, trigger: 'blur' }
-        ]
+        title: [{ required: true, message: '请输入名称', trigger: 'blur' }],
+        icon: [{ validator: checkIcon, trigger: 'blur' }],
+        url: [{ validator: checkUrl, trigger: 'blur' }]
       }
     }
   },
@@ -123,7 +143,7 @@ export default {
   },
   methods: {
     onSubmit () {
-      this.$refs.form.validate(valid => {
+      this.$refs.form.validate((valid) => {
         if (valid) {
           // 根据form数据中是否有id属性来判断当前是修改菜单还是添加菜单
           if (this.form.id && this.form.id > 0) {
@@ -132,49 +152,53 @@ export default {
           } else {
             // 添加
             this.addMenu()
-          }     
+          }
         }
       })
     },
     updateMenu () {
-      updateMenu(this.form).then(() => {
-        // 添加成功
-        // 显示添加成功的信息
-        this.$message.success({
-          message: '修改成功',
-          // 关闭对话框
-          onClose: () => {
-            this.dialogFormVisible = false
-          }
+      updateMenu(this.form)
+        .then(() => {
+          // 修改成功
+          // 显示添加成功的信息
+          this.$message.success({
+            message: '修改成功',
+            // 关闭对话框
+            onClose: () => {
+              this.dialogFormVisible = false
+            }
+          })
+          // 刷新列表数据
+          this.$store.dispatch('menu/getMenuList')
         })
-        // 刷新列表数据
-        this.$store.dispatch('menu/getMenuList')
-      }).catch(err => {
-        this.$message.error(err.message)
-      })
+        .catch((err) => {
+          this.$message.error(err.message)
+        })
     },
     addMenu () {
       // 处理菜单的添加,把表单的数据提交给接口
-      addMenu(this.form).then(() => {
-        // 添加成功
-        // 显示添加成功的信息
-        this.$message.success({
-          message: '添加成功',
-          // 关闭对话框
-          onClose: () => {
-            this.dialogFormVisible = false
-          }
+      addMenu(this.form)
+        .then(() => {
+          // 添加成功
+          // 显示添加成功的信息
+          this.$message.success({
+            message: '添加成功',
+            // 关闭对话框
+            onClose: () => {
+              this.dialogFormVisible = false
+            }
+          })
+          // 刷新列表数据
+          this.$store.dispatch('menu/getMenuList')
         })
-        // 刷新列表数据
-        this.$store.dispatch('menu/getMenuList')
-      }).catch(err => {
-        this.$message.error(err.message)
-      })
+        .catch((err) => {
+          this.$message.error(err.message)
+        })
     },
 
     clearForm () {
       // 把表单数据还原到初始值
-      this.form = {...defaultForm}
+      this.form = { ...defaultForm }
       // 清空所有的表单验证
       this.$refs.form.clearValidate()
     }
@@ -183,5 +207,4 @@ export default {
 </script>
 
 <style>
-
 </style>
