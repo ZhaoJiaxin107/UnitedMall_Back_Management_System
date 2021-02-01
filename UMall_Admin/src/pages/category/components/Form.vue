6af4986e7236ea63aef19308f09ef35a8613d611
Jiaxin Zhao <jzhao537@uwo.ca>
2021-02-01 15:11:31 +0800

    commit goods category

---
+++ UMall_Admin/src/pages/category/components/Form.vue
@@ -0,0 +1,180 @@
+<template>
+  <!-- el-dialog: 弹出对话框组件
+       title: 对话框标题
+       visible: 是否显示
+   -->
+  <el-dialog
+    :title="title"
+    :visible.sync="dialogFormVisible"
+    @close="clearForm"
+  >
+    <el-form
+      :model="form"
+      ref="form"
+      :rules="rules"
+      label-suffix="："
+      label-width="120px"
+    >
+      <el-form-item label="上级分类">
+        <el-select v-model="form.pid" placeholder="请选择上级分类">
+          <!-- 当value与v-model的值相等的时候就选中 -->
+          <!-- value与v-model是全等比较 -->
+          <el-option label="顶级分类" :value="0"></el-option>
+          <!-- 循环一级分类 -->
+          <!-- <el-option
+            v-for="item of firstMenuList"
+            :key="item.id"
+            :label="' |- ' + item.title"
+            :value="item.id"
+          >
+          </el-option> -->
+        </el-select>
+      </el-form-item>
+      <el-form-item label="分类名称" prop="title">
+        <el-input
+          v-model.trim="form.catename"
+          autocomplete="off"
+          placeholder="请输入分类名称"
+        >
+        </el-input>
+      </el-form-item>
+      <el-form-item label="图片">
+        <!-- 上传图片
+            list-type:文件列表的类型 text/picture/picture-card
+            auto-upload: 是否自动上传
+        -->
+        <el-upload action="#" list-type="picture-card" :auto-upload="false">
+          <i slot="default" class="el-icon-plus"></i>
+          <div slot="file" slot-scope="{ file }">
+            <!-- 预览的小图 -->
+            <img
+              class="el-upload-list__item-thumbnail"
+              :src="file.url"
+              alt=""
+            />
+            <!-- 预览大图片的按钮 -->
+            <span class="el-upload-list__item-actions">
+              <span
+                class="el-upload-list__item-preview"
+                @click="handlePictureCardPreview(file)"
+              >
+                <i class="el-icon-zoom-in"></i>
+              </span>
+              <!-- 删除图片的按钮 -->
+              <span
+                v-if="!disabled"
+                class="el-upload-list__item-delete"
+                @click="handleRemove(file)"
+              >
+                <i class="el-icon-delete"></i>
+              </span>
+            </span>
+          </div>
+        </el-upload>
+        <!-- 展示大图片的对话框 -->
+        <el-dialog :visible.sync="dialogVisible">
+          <img width="100%" :src="dialogImageUrl" alt="" />
+        </el-dialog>
+      </el-form-item>
+      <el-form-item label="状态">
+        <!-- 开关组件
+            active-value: Boolean | string | number 选中时的值
+            inactive-value: Boolean | string | number 未选中时的值
+        -->
+        <el-switch v-model="form.status" :active-value="1" :inactive-value="2">
+        </el-switch>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="onSubmit">确 定</el-button>
+      </el-form-item>
+    </el-form>
+  </el-dialog>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+// 引入接口方法
+// 导出所有的非default内容
+// import { addMenu, updateMenu } from '@/api/menu'
+import * as model from '@/api/menu'
+const defaultForm = {
+  pid: 0, // 上级分类编号, 顶级菜单为0
+  catename: '', // 分类的名称
+  img: '', // 分类的图片
+  status: 1 // 状态 1正常 2禁用
+}
+export default {
+  data () {
+    return {
+      dialogFormVisible: true,
+      title: '', // 对话框的标题
+      form: { ...defaultForm },
+      rules: {
+        catename: [
+          { required: true, message: '请输入分类名称', trigger: 'blur' }
+        ]
+      },
+      dialogVisible: false, // 是否展示大图片
+      dialogImageUrl: '' //  大图片的地址
+    }
+  },
+  computed: {
+    ...mapGetters({
+      firstMenuList: 'menu/firstMenuList'
+    })
+  },
+  methods: {
+    // 图片预览(展示大图)
+    handlePictureCardPreview (file) {
+      console.log(file)
+    },
+    handleRemove (file) {
+      console.log(file)
+    },
+    onSubmit () {
+      this.$refs.form.validate((valid) => {
+        if (valid) {
+          // 根据form数据中是否有id属性来判断当前是修改菜单还是添加菜单
+          if (this.form.id && this.form.id > 0) {
+            // 修改
+            this.editMenu('updateMenu')
+          } else {
+            // 添加
+            this.editMenu()
+          }
+        }
+      })
+    },
+    editMenu (method = 'addMenu') {
+      // 处理菜单的添加,把表单的数据提交给接口
+      model[method](this.form)
+        .then(() => {
+          // 添加成功
+          // 显示添加成功的信息
+          this.$message.success({
+            message: method === 'addMenu' ? '添加成功' : '修改成功',
+            // 关闭对话框
+            onClose: () => {
+              this.dialogFormVisible = false
+            }
+          })
+          // 刷新列表数据
+          this.$store.dispatch('menu/getMenuList')
+        })
+        .catch((err) => {
+          this.$message.error(err.message)
+        })
+    },
+    clearForm () {
+      // 把表单数据还原到初始值
+      this.form = { ...defaultForm }
+      // 清空所有的表单验证
+      this.$refs.form.clearValidate()
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ UMall_Admin/src/pages/category/components/List.vue
@@ -0,0 +1,115 @@
+<template>
+<!--
+    树形数据表格
+    data: 要显示的数据
+    row-key: 循环的tr的key值 对应的菜单数据的编号
+    border: 是否添加边框
+    default-expand-all: 是否展开所有的数据
+    tree-props: 下级数据的结构 {children(表格组件属性): 'children(表单数据对应的下级菜单)'}
+-->
+ <el-table
+    :data="list"
+    style="width: 100%;margin: 20px 0;"
+    row-key="id"
+    border
+    height="400"
+    default-expand-all
+    :tree-props="{children: 'children'}">
+    <!-- el-table-column:表格的列组件
+        prop: 菜单数据中对应的数据
+        label:表头
+         -->
+    <el-table-column
+      prop="title"
+      label="菜单名称">
+    </el-table-column>
+    <el-table-column
+      label="菜单类型">
+    <template #default = "props">
+      <el-tag>{{props.row.type === 1 ? '目录':'菜单'}}</el-tag>
+    </template>
+    </el-table-column>
+    <el-table-column
+      label="菜单图标">
+    <template #default = "props">
+      <i v-if="props.row.icon !== ''" :class="props.row.icon"></i>
+    </template>
+    </el-table-column>
+    <el-table-column
+      prop="url"
+      label="菜单地址">
+    </el-table-column>
+    <el-table-column
+      label="状态">
+    <template #default = "props">
+      <el-tag v-if = "props.row.status === 1">启用</el-tag>
+      <el-tag v-else type="danger">禁用</el-tag>
+    </template>
+    </el-table-column>
+    <el-table-column
+      label="操作">
+    <template #default = "props">
+       <el-button type="primary" size = "mini" @click="onEdit(props.row)"><i class="el-icon-edit"></i> 编辑</el-button>
+       <el-button type="danger" size = "mini" @click="onDelete(props.row)"><i class="el-icon-delete"></i> 删除</el-button>
+    </template>
+    </el-table-column>
+  </el-table>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import { deleteMenu } from '@/api/menu'
+export default {
+  computed: {
+    ...mapState({
+      list: state => state.menu.list
+    })
+  },
+  mounted () {
+    if (this.list.length === 0) {
+      this.$store.dispatch('menu/getMenuList')
+    }
+  },
+  methods: {
+    onEdit (data) {
+    //   console.log('on edit...')
+    // 触发编辑按钮
+    // 通知父组件显示编辑菜单的对话框, 把当前编辑的数据传递过去
+      this.$emit('edit', data)
+    },
+    onDelete (data) {
+      // element-ui的弹出框this.$comfirm(显示的信息[,标题,其他的配置项目])
+      this.$confirm('确定要删除吗?', '提示', { type: 'error' }).then(() => {
+        // 完成删除功能
+        // 如果有下级菜单，就不允许删除
+        if (Reflect.has(data, 'children') && data.children.length > 0) {
+          this.$message.error('有下级菜单, 不允许删除')
+          return
+        }
+        // 调用接口删除菜单
+        // console.log('删除')
+        deleteMenu(data.id).then(() => {
+          // 刷新列表数据
+          this.$message.success({
+            message: '删除成功',
+            onClose: () => {
+              // 关闭对话框
+              this.dialogFormVisible = false
+              // 刷新列表数据
+              this.$store.dispatch('menu/getMenuList')
+            }
+          })
+        }).catch(err => {
+          this.$message.error(err.message)
+        })
+        // 调用接口删除菜单
+        // 刷新列表数据
+      }).catch(() => {})
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
