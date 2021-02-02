87c5fb47aa51ec3e494e65e9e3fcd9861eed1594
Jiaxin Zhao <jzhao537@uwo.ca>
2021-02-02 22:19:39 +0800

    update specs.js

--- UMall_Admin/src/api/specs.js
+++ UMall_Admin/src/api/specs.js
@@ -7,11 +7,11 @@ export const addSpecs = (data) => {
 
 // 修改规格
 export const updateSpecs = (data) => {
-  // 判断data中是否包含id属性且大于0
+// 判断data中是否包含id属性且大于0
   if (!Reflect.has(data, 'id') || data.id <= 0) {
     return Promise.reject(new Error('缺少ID参数或id参数错误'))
   }
-  return http.post('/specsedit')
+  return http.post('/specsedit', data)
 }
 
 // 分页获取规格数据
