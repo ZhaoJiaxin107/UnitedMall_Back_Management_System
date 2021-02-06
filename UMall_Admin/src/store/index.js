64cd84abb892c707c9a6e3c0266542ac519de642
Jiaxin Zhao <jzhao537@uwo.ca>
2021-02-06 20:39:02 +0800

    add goods in vuex

--- UMall_Admin/src/store/index.js
+++ UMall_Admin/src/store/index.js
@@ -7,6 +7,7 @@ import role from './modules/role'
 import admin from './modules/admin'
 import category from './modules/category'
 import specs from './modules/specs'
+import goods from './modules/goods'
 // 使用vuex插件
 Vue.use(Vuex)
 
@@ -17,7 +18,8 @@ const store = new Vuex.Store({
     role,
     admin,
     category,
-    specs
+    specs,
+    goods
   }
 })
 
