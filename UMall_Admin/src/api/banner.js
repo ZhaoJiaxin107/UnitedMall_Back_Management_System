4bed34c083f38ed9abefda0f1daa06087ce4ec3a
Jiaxin Zhao <jzhao537@uwo.ca>
2021-02-07 15:18:54 +0800

    create banner.js for api

---
+++ UMall_Admin/src/api/banner.js
@@ -0,0 +1,25 @@
+import http from './http'
+
+// 添加轮播图
+export const addBanner = (data) => {
+  return http.post('/banneradd', data)
+}
+
+// 获取轮播图列表
+export const getBannerList = () => {
+  return http.get('/bannerlist')
+}
+
+// 修改轮播图
+export const updateBanner = (data) => {
+  // 判断data中否包含id属性且大于0
+  if (!Reflect.has(data, 'id') || data.id <= 0) {
+    return Promise.reject(new Error('缺少ID参数或ID参数错误'))
+  }
+  return http.post('/banneredit', data)
+}
+
+// 删除轮播图
+export const deleteBanner = (id) => {
+  return http.post('/bannerdelete', { id })
+}
